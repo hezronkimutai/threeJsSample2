3798d9ccfdf7f2eceacdcc375aefd1280ad7030a
Hezron Chelimo <hezronchelimo.hc@gmail.com>
2022-07-24 05:31:57 +0300

    a study of gtlf, stl and fbx loaders

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.SpotLight()
-light.position.set(20, 20, 20)
-scene.add(light)
+
+const light1 = new THREE.SpotLight()
+light1.position.set(-20, -20, -20)
+scene.add(light1)
+
+const light2 = new THREE.SpotLight()
+light2.position.set(20, 20, 20)
+scene.add(light2)
+
+const light3 = new THREE.SpotLight()
+light3.position.set(-20, -20, -20)
+scene.add(light3)
+
+const light4 = new THREE.SpotLight()
+light4.position.set(-40, 40, 40)
+scene.add(light4)
+
+const light5 = new THREE.SpotLight()
+light5.position.set(20, -20, 20)
+scene.add(light5)
+
+
+
 
 const camera = new THREE.PerspectiveCamera(
-  75,
+  30,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 )
-camera.position.z = 3
+camera.position.z = 20
+camera.position.y = -20
+camera.position.x = 20
 
 const renderer = new THREE.WebGLRenderer()
 renderer.outputEncoding = THREE.sRGBEncoding
@@ -37,7 +61,7 @@ const envTexture = new THREE.CubeTextureLoader().load([
 envTexture.mapping = THREE.CubeReflectionMapping
 
 const material = new THREE.MeshPhysicalMaterial({
-  color: 0xffffc8,
+  color: 0xffffff,
   envMap: envTexture,
   metalness: 0.25,
   roughness: 0.1,
@@ -48,12 +72,35 @@ const material = new THREE.MeshPhysicalMaterial({
   clearcoatRoughness: 0.25
 })
 
-const loader = new STLLoader()
-loader.load(
-  '../hezronkimutai-2021.stl',
-  function (geometry) {
-    const mesh = new THREE.Mesh(geometry, material)
-    scene.add(mesh)
+// const loader = new STLLoader()
+// loader.load(
+//   '../hezronkimutai-2021.stl',
+//   function (geometry) {
+//     const mesh = new THREE.Mesh(geometry, material)
+//     scene.add(mesh)
+//   },
+//   (xhr) => {
+//     console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+//   },
+//   (error) => {
+//     console.log(error)
+//   }
+// )
+
+const fbxLoader = new FBXLoader()
+fbxLoader.load(
+  '../scene.fbx',
+  (object) => {
+    // object.traverse(function (child) {
+    //     if ((child as THREE.Mesh).isMesh) {
+    //         // (child as THREE.Mesh).material = material
+    //         if ((child as THREE.Mesh).material) {
+    //             ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
+    //         }
+    //     }
+    // })
+    // object.scale.set(.01, .01, .01)
+    scene.add(object)
   },
   (xhr) => {
     console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
