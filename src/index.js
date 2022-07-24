5a1bd0b28da9bc46e4d053e9fe454895b956cb11
Hezron Chelimo <hezronchelimo.hc@gmail.com>
2022-07-24 09:52:08 +0300

    auto rotation around an axis

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
 import Stats from 'three/examples/jsm/libs/stats.module'
@@ -29,7 +28,7 @@ const light5 = new THREE.SpotLight()
 light5.position.set(20, -20, 20)
 scene.add(light5)
 
-
+let object1;
 
 
 const camera = new THREE.PerspectiveCamera(
@@ -38,10 +37,9 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 )
-camera.position.z = 20
-camera.position.y = -20
-camera.position.x = 20
-
+camera.position.z = 20;
+camera.position.y = -20;
+camera.position.x = 20;
 const renderer = new THREE.WebGLRenderer()
 renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -49,66 +47,41 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
+// controls.autoRotate = true
+// controls.autoRotateSpeed = 10;
+
+
 
 const envTexture = new THREE.CubeTextureLoader().load([
-  'img/px_50.png',
-  'img/nx_50.png',
-  'img/py_50.png',
-  'img/ny_50.png',
-  'img/pz_50.png',
-  'img/nz_50.png'
+  '../160920-irish-2.jpg',
+  '../160920-irish-2.jpg',
+  '../160920-irish-2.jpg',
+  '../160920-irish-2.jpg',
+  '../160920-irish-2.jpg',
+  '../160920-irish-2.jpg',
 ])
 envTexture.mapping = THREE.CubeReflectionMapping
 
-const material = new THREE.MeshPhysicalMaterial({
-  color: 0xffffff,
-  envMap: envTexture,
-  metalness: 0.25,
-  roughness: 0.1,
-  opacity: 1.0,
-  transparent: true,
-  transmission: 0.99,
-  clearcoat: 1.0,
-  clearcoatRoughness: 0.25
-})
+// const material = new THREE.MeshPhysicalMaterial({
+//   color: 0xffffff,
+//   envMap: envTexture,
+//   metalness: 0.25,
+//   roughness: 0.1,
+//   opacity: 1.0,
+//   transparent: true,
+//   transmission: 0.99,
+//   clearcoat: 1.0,
+//   clearcoatRoughness: 0.25
+// })
 
-// const loader = new STLLoader()
-// loader.load(
-//   '../hezronkimutai-2021.stl',
-//   function (geometry) {
-//     const mesh = new THREE.Mesh(geometry, material)
-//     scene.add(mesh)
-//   },
-//   (xhr) => {
-//     console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-//   },
-//   (error) => {
-//     console.log(error)
-//   }
-// )
+// var geometry = new THREE.BoxGeometry(1, 1, 1);
 
-const fbxLoader = new FBXLoader()
-fbxLoader.load(
-  '../scene.fbx',
-  (object) => {
-    // object.traverse(function (child) {
-    //     if ((child as THREE.Mesh).isMesh) {
-    //         // (child as THREE.Mesh).material = material
-    //         if ((child as THREE.Mesh).material) {
-    //             ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
-    //         }
-    //     }
-    // })
-    // object.scale.set(.01, .01, .01)
-    scene.add(object)
-  },
-  (xhr) => {
-    console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-  },
-  (error) => {
-    console.log(error)
-  }
-)
+// var cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+
+
+
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -117,15 +90,44 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight)
   render()
 }
+controls.update();
 
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
+var axis = new THREE.Vector3(4, 0, 7).normalize();
+var speed = 0.01;
+let index = 0;
+
 function animate() {
   requestAnimationFrame(animate)
 
-  controls.update()
+  const fbxLoader = new FBXLoader()
+  fbxLoader.load(
+    '../scene.fbx',
+    (object) => {
+      object.name = `object_${index}`;
+      scene.add(object)
+      try {
+        if (index) {
+          var selectedObject = scene.getObjectByName(`object_${index - 1}`);
+          console.log({ selectedObject, index, object, scene });
+          scene.remove(selectedObject);
+        }
+        index++;
 
+      } catch (error) {
+
+      }
+      object.rotateOnAxis(axis, speed += 0.01);
+    },
+    (xhr) => {
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+      console.log(error)
+    }
+  )
   render()
 
   stats.update()
